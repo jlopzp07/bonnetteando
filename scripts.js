5a9fc8682a6f278ffb9cc62ab56ed1d8cd94439f
jlopzp07 <jlopezp07@gmail.com>
2022-12-14 13:16:25 -0500

    fixed mobile navbar, it was not closing. Fixed width in journey page, fixed all links in mobile navbar

--- scripts.js
+++ scripts.js
@@ -3,3 +3,9 @@ menu.onclick = function () {
     navMenu = document.querySelector(".nav-menu");
     navMenu.classList.toggle("active");
 }
+
+close = document.querySelector(".close");
+close.onclick = function () {
+    navMenu = document.querySelector(".nav-menu");
+    navMenu.classList.toggle("active");
+}
